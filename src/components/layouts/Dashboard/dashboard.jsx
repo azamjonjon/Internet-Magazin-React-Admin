8bfc1f5503f87a47f45402ed63f6923870b862b1
ismoil3 <nasridinovismoil945@gmail.com>
2025-02-24 17:28:50 +0500

    first commit

---
+++ src/components/layouts/Dashboard/dashboard.jsx
@@ -0,0 +1,64 @@
+import { extendTheme } from "@mui/material";
+import { AppProvider, DashboardLayout, PageContainer } from "@toolpad/core";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import LayersIcon from "@mui/icons-material/Layers";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+const NAVIGATION = [
+  { kind: "header", title: "Main items" },
+  { segment: "", title: "Dasboard", icon: <DashboardIcon /> },
+  { segment: "orders", title: "Orders", icon: <ShoppingCartIcon /> },
+  { segment: "products", title: "Products", icon: <LayersIcon /> },
+  { segment: "other", title: "Other", icon: <BarChartIcon /> },
+];
+
+const demoTheme = extendTheme({
+  colorSchemes: { light: true, dark: true },
+  colorSchemeSelector: "class",
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function useDemoRouter() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return {
+    pathname: location.pathname,
+    searchParams: new URLSearchParams(location.search),
+    navigate, 
+  };
+}
+
+const Dashbord = () => {
+  const router = useDemoRouter("/dashboard");
+  
+  const demoWindow = typeof window !== "undefined" ? window : undefined;
+
+  return (
+    <AppProvider
+      navigation={NAVIGATION}
+      router={router}
+      theme={demoTheme}
+      window={demoWindow}
+    >
+      <DashboardLayout>
+        <PageContainer>
+          {/* Child routes will render here */}
+          <Outlet />
+        </PageContainer>
+      </DashboardLayout>
+    </AppProvider>
+  );
+};
+
+export default Dashbord;
